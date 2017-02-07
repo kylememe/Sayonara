f161d1ec8c508f4063093313bba93e2135ac609d
Kyle Smith <kylesmith6568@gmail.com>
2017-02-06 22:42:09 -0500

    Redo for latest version of .net core

---
+++ src/Sayonara/Models/SayonaraContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sayonara.Models
+{	
+
+	public class SayonaraContext : DbContext
+  {
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			
+    }
+    
+		public DbSet<Extract> Extracts { get; set; }		
+	}
+}
