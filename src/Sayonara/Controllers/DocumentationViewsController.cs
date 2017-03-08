cb5ecedb9662d4029d746bba0410d7d65a54308c
Kyle Smith <kylesmith6568@gmail.com>
2017-03-07 22:37:04 -0500

    Started to get documentation views loaded

---
+++ src/Sayonara/Controllers/DocumentationViewsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sayonara.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sayonara.Controllers
+{
+	[Route("api/[controller]")]
+	public class DocumentationViewsController : Controller
+	{
+		private Sayonara.Data.SayonaraContext _sayonaraContext;
+
+		public DocumentationViewsController(Sayonara.Data.SayonaraContext context)
+		{
+			_sayonaraContext = context;
+		}
+
+		// GET: api/values
+		[HttpGet]
+		public async Task<IActionResult> Get(int facilityID)
+		{			
+			var facility = await _sayonaraContext.Facilities.Include("DocumentationViews")
+				.Where(f => f.ID == facilityID)
+				.SingleAsync();
+
+			var views = new List<DocumentationView>();
+
+			foreach (var view in facility.DocumentationViews)
+				views.Add(new DocumentationView()
+				{
+					ID = view.ID,
+					FacilityID = view.FacilityID,
+					MedicalRecordCopy = view.MedicalRecordCopy,
+					Name = view.Name
+				});						
+
+			return Ok(views);
+		}
+
+	}
+}
