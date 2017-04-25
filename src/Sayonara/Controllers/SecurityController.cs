8bb2bffac4d3770a601bdf489fa3d7e5ce1d86fe
Kyle Smith <kylesmith6568@gmail.com>
2017-04-24 22:37:52 -0400

    Added placeholders for security stuff

---
+++ src/Sayonara/Controllers/SecurityController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sayonara.Controllers
+{
+    public class SecurityController : Controller
+    {
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		public IActionResult Logout()
+		{			
+			return View();
+		}
+	}
+}
